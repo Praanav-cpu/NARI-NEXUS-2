6040ce6b6ef8ed96f61ef16e80eb388581631398
Praanav-cpu <pranavtekade250107@gmail.com>
2025-04-12 09:32:34 +0530

    upload on git hub

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import Programs from './pages/Programs'
+import Events from './pages/Events'
+import Resources from './pages/Resources'
+import Stories from './pages/Stories'
+import Contact from './pages/Contact'
+import Team from './pages/Team'
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col bg-black text-white">
+      <Navbar />
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/stories" element={<Stories />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
