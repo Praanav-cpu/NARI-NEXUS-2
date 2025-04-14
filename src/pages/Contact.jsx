d1ac1d580b014f43be61690d291619e135eac720
Praanav-cpu <pranavtekade250107@gmail.com>
2025-04-15 00:06:05 +0530

    contact us ready

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -133,7 +133,7 @@ const Contact = () => {
                   inspiration.
                 </p>
                 <div className="flex space-x-4">
-                  <a
+                  {/* <a
                     href="#"
                     className="w-10 h-10 rounded-full bg-white/10 flex items-center justify-center hover:bg-white/20 transition-colors"
                   >
@@ -145,9 +145,9 @@ const Contact = () => {
                     >
                       <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
                     </svg>
-                  </a>
+                  </a> */}
                   <a
-                    href="#"
+                    href="https://x.com/narinexus"
                     className="w-10 h-10 rounded-full bg-white/10 flex items-center justify-center hover:bg-white/20 transition-colors"
                   >
                     <svg
@@ -173,7 +173,7 @@ const Contact = () => {
                     </svg>
                   </a>
                   <a
-                    href="#"
+                    href="https://www.linkedin.com/company/narinexus/"
                     className="w-10 h-10 rounded-full bg-white/10 flex items-center justify-center hover:bg-white/20 transition-colors"
                   >
                     <svg
