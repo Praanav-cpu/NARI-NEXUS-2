e2485cc14ed9ad286b55d5b4598708f0a37b859a
Praanav-cpu <pranavtekade250107@gmail.com>
2025-04-28 11:50:41 +0530

    new update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -522,7 +522,7 @@ const Home = () => {
 
       {/* Programs Section */}
       <section
-        id="programs"
+      
         className="py-20 relative bg-gradient-to-b from-black to-purple-900/20 scroll-fade"
       >
         <GeometricBackground className="opacity-30" />
@@ -591,15 +591,15 @@ const Home = () => {
         </div>
       </section>
 
-      <section id="programs" className="py-20 relative scroll-fade">
+      {/* <section id="programs" className="py-20 relative scroll-fade">
         <GeometricBackground2 className="opacity-50" />
         <div className="container mx-auto px-4">
-          <SectionTitle title="Our Mission" centered={true} />
+          <SectionTitle title="Our Mission" centered={true} /> */}
 
           {/* Programming language logos background */}
-          <div className="absolute inset-0 z-0 overflow-hidden opacity-10">
+          {/* <div className="absolute inset-0 z-0 overflow-hidden opacity-10"> */}
             {/* JavaScript logo */}
-            <div className="absolute top-10 left-20 w-16 h-16">
+            {/* <div className="absolute top-10 left-20 w-16 h-16">
               <svg viewBox="0 0 128 128">
                 <path fill="#F0DB4F" d="M1.408 1.408h125.184v125.185H1.408z" />
                 <path
@@ -607,10 +607,10 @@ const Home = () => {
                   d="M116.347 96.736c-.917-5.711-4.641-10.508-15.672-14.981-3.832-1.761-8.104-3.022-9.377-5.926-.452-1.69-.512-2.642-.226-3.665.821-3.32 4.784-4.355 7.925-3.403 2.023.678 3.938 2.237 5.093 4.724 5.402-3.498 5.391-3.475 9.163-5.879-1.381-2.141-2.118-3.129-3.022-4.045-3.249-3.629-7.676-5.498-14.756-5.355l-3.688.477c-3.534.893-6.902 2.748-8.877 5.235-5.926 6.724-4.236 18.492 2.975 23.335 7.104 5.332 17.54 6.545 18.873 11.531 1.297 6.104-4.486 8.08-10.234 7.378-4.236-.881-6.592-3.034-9.139-6.949-4.688 2.713-4.688 2.713-9.508 5.485 1.143 2.499 2.344 3.63 4.26 5.795 9.068 9.198 31.76 8.746 35.83-5.176.165-.478 1.261-3.666.38-8.581zM69.462 58.943H57.753l-.048 30.272c0 6.438.333 12.34-.714 14.149-1.713 3.558-6.152 3.117-8.175 2.427-2.059-1.012-3.106-2.451-4.319-4.485-.333-.584-.583-1.036-.667-1.071l-9.52 5.83c1.583 3.249 3.915 6.069 6.902 7.901 4.462 2.678 10.459 3.499 16.731 2.059 4.082-1.189 7.604-3.652 9.448-7.401 2.666-4.915 2.094-10.864 2.07-17.444.06-10.735.001-21.468.001-32.237z"
                 />
               </svg>
-            </div>
+            </div> */}
 
             {/* Python logo */}
-            <div className="absolute top-40 right-24 w-20 h-20">
+            {/* <div className="absolute top-40 right-24 w-20 h-20">
               <svg viewBox="0 0 128 128">
                 <linearGradient
                   id="python-original-a"
@@ -647,20 +647,20 @@ const Home = () => {
                   transform="translate(0 10.26)"
                 />
               </svg>
-            </div>
+            </div> */}
 
             {/* Node.js logo */}
-            <div className="absolute bottom-32 left-40 w-16 h-16">
+            {/* <div className="absolute bottom-32 left-40 w-16 h-16">
               <svg viewBox="0 0 128 128">
                 <path
                   fill="#83CD29"
                   d="M112.771 30.334L68.674 4.729c-2.781-1.584-6.402-1.584-9.205 0L14.901 30.334C12.031 31.985 10 35.088 10 38.407v51.142c0 3.319 2.084 6.423 4.954 8.083l11.775 6.688c5.628 2.772 7.617 2.772 10.178 2.772 8.333 0 13.093-5.039 13.093-13.828v-50.49c0-.713-.371-1.774-1.071-1.774h-5.623C42.594 41 41 42.061 41 42.773v50.49c0 3.896-3.524 7.773-10.11 4.48L18.723 90.73c-.424-.23-.723-.693-.723-1.181V38.407c0-.482.555-.966.982-1.213l44.424-25.561c.415-.235 1.025-.235 1.439 0l43.882 25.555c.42.253.272.722.272 1.219v51.142c0 .488.183.963-.232 1.198l-44.086 25.576c-.378.227-.847.227-1.261 0l-11.307-6.749c-.341-.198-.746-.269-1.073-.086-3.146 1.783-3.726 2.02-6.677 3.043-.726.253-1.797.692.41 1.929l14.798 8.754a9.294 9.294 0 004.647 1.246c1.642 0 3.25-.426 4.667-1.246l43.885-25.582c2.87-1.672 4.23-4.764 4.23-8.083V38.407c0-3.319-1.36-6.414-4.229-8.073zM77.91 81.445c-11.726 0-14.309-3.235-15.17-9.066-.1-.628-.633-1.379-1.272-1.379h-5.731c-.709 0-1.279.86-1.279 1.566 0 7.466 4.059 16.512 23.453 16.512 14.039 0 22.088-5.455 22.088-15.109 0-9.572-6.467-12.084-20.082-13.886-13.762-1.819-15.16-2.738-15.16-5.962 0-2.658 1.184-6.203 11.374-6.203 9.105 0 12.461 1.954 13.842 8.091.118.577.645.991 1.24.991h5.754c.354 0 .692-.143.94-.396.24-.272.367-.613.335-.979-.891-10.568-7.912-15.493-22.112-15.493-12.631 0-20.166 5.334-20.166 14.275 0 9.698 7.497 12.378 19.622 13.577 14.505 1.422 15.633 3.542 15.633 6.395 0 4.955-3.978 7.066-13.309 7.066z"
                 />
               </svg>
-            </div>
+            </div> */}
 
             {/* Java logo */}
-            <div className="absolute top-60 left-1/2 w-16 h-16">
+            {/* <div className="absolute top-60 left-1/2 w-16 h-16">
               <svg viewBox="0 0 128 128">
                 <path
                   fill="#0074BD"
@@ -683,10 +683,10 @@ const Home = () => {
                   d="M52.214 126.021c22.476 1.437 57-.8 57.817-11.436 0 0-1.571 4.032-18.577 7.231-19.186 3.612-42.854 3.191-56.887.874 0 .001 2.875 2.381 17.647 3.331z"
                 />
               </svg>
-            </div>
+            </div> */}
 
             {/* Bootstrap logo */}
-            <div className="absolute bottom-20 right-32 w-16 h-16">
+            {/* <div className="absolute bottom-20 right-32 w-16 h-16">
               <svg viewBox="0 0 128 128">
                 <path
                   fill="#5B4282"
@@ -764,10 +764,10 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* Events Section */}
-      <section className="py-20 relative bg-gradient-to-b from-black to-purple-900/20 scroll-fade">
+      <section id= 'programs' className="py-20 relative bg-gradient-to-b from-black to-purple-900/20 scroll-fade">
         <SectionTitle title="Upcoming Events" centered={true} />
         <GeometricBackground className="opacity-30" />
 
@@ -1055,9 +1055,10 @@ const Home = () => {
               <Button to="/Contact" size="xl">
                 Get Started
               </Button>
-              <Button to="#programs" size="xl">
-                Programs
-              </Button>
+              <Button as="a" href="#programs" size="xl">
+  Events
+</Button>
+
             </div>
           </div>
         </div>
